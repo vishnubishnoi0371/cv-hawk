677211237d8301ae6a0a3b14e7859b56d80bb3be
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2023-11-09 10:53:52 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,75 @@
+
+let a = document.getElementById("nav");
+let flow = document.querySelector(".flow");
+let cross4 = document.querySelector(".cross-4");
+let cross5 = document.querySelector(".cross-5");
+let cross6 = document.querySelector(".cross-6");
+let menuicon = document.querySelector("#menuIcon");
+let Home = document.querySelector(".Home");
+let About = document.querySelector(".About");
+let Help = document.querySelector(".Help");
+let Test = document.querySelector(".Test");
+let myBtn = document.querySelector(".myBtn");
+
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myBtn.style.display = "block";
+    } else {
+        myBtn.style.display = "none";
+    }
+}
+
+myBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+
+menuicon.addEventListener("click", function () {
+    if (a.classList !== document.querySelector("fix-lft")) {
+        a.classList.toggle("fix-lft");
+        flow.classList.toggle("flow-hidden");
+        cross4.classList.toggle("cross-1");
+        cross5.classList.toggle("cross-2");
+        cross6.classList.toggle("cross-3")
+    }
+});
+
+Home.addEventListener("click", function () {
+    a.classList.toggle("fix-lft");
+    flow.classList.remove("flow-hidden");
+    cross4.classList.toggle("cross-1");
+    cross5.classList.toggle("cross-2");
+    cross6.classList.toggle("cross-3");
+});
+About.addEventListener("click", function () {
+    a.classList.remove("fix-lft");
+    flow.classList.remove("flow-hidden");
+    cross4.classList.toggle("cross-1");
+    cross5.classList.toggle("cross-2");
+    cross6.classList.toggle("cross-3");
+});
+Help.addEventListener("click", function () {
+    a.classList.remove("fix-lft");
+    flow.classList.remove("flow-hidden");
+    cross4.classList.toggle("cross-1");
+    cross5.classList.toggle("cross-2");
+    cross6.classList.toggle("cross-3");
+});
+
+test.addEventListener("click", function () {
+    a.classList.remove("fix-lft");
+    flow.classList.remove("flow-hidden");
+    cross4.classList.toggle("cross-1");
+    cross5.classList.toggle("cross-2");
+    cross6.classList.toggle("cross-3");
+});
+
+
+const preloder = document.getElementById("preloder")
+setTimeout(() => {
+    document.getElementById("preloder").classList.add("d-none");
+    document.body.classList.remove("overflow-hidden")
+}, 2000)
+
